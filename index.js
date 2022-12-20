6037545fe6281d0b21d534d2bc4155aaf58ec23e
borges2carlos <117941123+borges2carlos@users.noreply.github.com>
2022-12-20 09:03:04 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import 'react-native-gesture-handler';
+import { registerRootComponent } from 'expo';
+import './src/config/ReactotronConfig';
+import moment from 'moment';
+import 'moment/locale/pt-br';
+
+moment.locale('pt-br')
+
+import App from './src';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
